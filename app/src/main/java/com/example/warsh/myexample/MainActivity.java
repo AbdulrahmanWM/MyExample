fdc5f24eb0617e2188880e1f7b2516e45aec9be1
Abdulrahman <s201324890@kfupm.edu.sa>
2018-03-06 20:37:22 +0300

    I Added This Method

--- app/src/main/java/com/example/warsh/myexample/MainActivity.java
+++ app/src/main/java/com/example/warsh/myexample/MainActivity.java
@@ -10,7 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    public void ahmedMethod(){return;}
     public void abdMethod(){
         return;
         //meow
